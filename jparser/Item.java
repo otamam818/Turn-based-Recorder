cf73e3602ded140500e94df317f89a15a89ba918
otamam818 <otamam818@gmail.com>
2022-02-18 07:49:28 +1100

    Added data recorders

--- jparser/Item.java
+++ jparser/Item.java
@@ -1,10 +1,15 @@
 package jparser;
 
 public class Item {
+    // The name of the item
     private String Name;
+    // information describing the item
     private String Description;
+    // Its minimum cost range from the available alternatives
     private float minCostRange;
+    // Its maximum cost range from the available alternatives
     private float maxCostRange;
+    // The number of times this item is bought
     private int timesBought;
 
     public Item (String Name, String Description, float minCostRange, float maxCostRange) {

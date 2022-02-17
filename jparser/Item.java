37cfd0df2bff01b2c8596f7a0dd2b484cce2244f
otamam818 <otamam818@gmail.com>
2022-02-18 00:06:11 +1100

    Added variables and constructors

--- jparser/Item.java
+++ jparser/Item.java
@@ -1,6 +1,19 @@
 package jparser;
 
 public class Item {
+    private String Name;
+    private String Description;
+    private float minCostRange;
+    private float maxCostRange;
+    private int timesBought;
+
+    public Item (String Name, String Description, float minCostRange, float maxCostRange) {
+        this.Name = Name;
+        this.Description = Description;
+        this.minCostRange = minCostRange;
+        this.maxCostRange = maxCostRange;
+    }
+
     public static void main(String args[]) {
         System.out.println("Hello world");
     }

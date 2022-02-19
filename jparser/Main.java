aebe712f75a582646777ff47ce16f90815130839
otamam818 <otamam818@gmail.com>
2022-02-19 17:49:58 +1100

    Added Main class for executing CLI logic

---
+++ jparser/Main.java
@@ -0,0 +1,10 @@
+package jparser;
+import java.util.ArrayList;
+
+public class Main {
+    ArrayList <Item> allItems;
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
+

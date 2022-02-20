490767003a4f3939e10e48f9c0555f2f613fa844
otamam818 <otamam818@gmail.com>
2022-02-20 21:01:24 +1100

    Added extra attributes and made toString skeleton

--- jparser/Item.java
+++ jparser/Item.java
@@ -2,6 +2,8 @@ package jparser;
 
 /* Parses Items and sorts them by name */
 public class Item {
+    // The filename of the item; its unique identifier
+    private String ID;
     // The name of the item
     private String Name;
     // information describing the item
@@ -14,6 +16,7 @@ public class Item {
     private int timesBought;
 
     public Item (String Name, String Description, float minCostRange, float maxCostRange) {
+        this.ID           = ID;
         this.Name         = Name;
         this.Description  = Description;
         this.minCostRange = minCostRange;
@@ -22,6 +25,26 @@ public class Item {
 
     public static void main(String args[]) {
         System.out.println("Hello world");
+    }
+
+    public String toString() {
+        // Format:
+        //      - ID
+        //      - Name
+        //      - Description
+        //      - minCostRange
+        //      - maxCostRange
+        //   separated by ' \| '
+        final String SEPARATOR = " \\| ";
+        String[] items = [
+            this.ID,
+            this.Name,
+            this.Description,
+            this.minCostRange,
+            this.maxCostRange
+        ];
+        String result = "";
+        return result;
     }
 }
 

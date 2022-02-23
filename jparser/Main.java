57c4de7a25455f0440eda93cb27c55bea637f62b
otamam818 <otamam818@gmail.com>
2022-02-23 18:00:37 +1100

    Tested Item.class implementation, changed allItems variable logic, and added skeleton for loading items

--- jparser/Main.java
+++ jparser/Main.java
@@ -1,10 +1,23 @@
 package jparser;
 import java.util.ArrayList;
+import java.io.File;
 
 public class Main {
-    ArrayList <Item> allItems;
+    public final ArrayList <Item> allItems = new ArrayList <>();
+    public final File DIR_ITEMS = new File("data/items");
+
     public static void main(String args[]) {
+        Item test = new Item("01", "Bananas", "Edible", 12f, 16f);
         System.out.println("Hello world");
+        System.out.println(test.toString());
+    }
+
+    ArrayList <Item> loadItems(String DIR_ITEMS) {
+        ArrayList <Item> finList = new ArrayList <Item>();
+        // check if there are any items
+        // If there are, return the item
+        // if not, return '-1'
+        return finList;
     }
 }
 

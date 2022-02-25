9980231d52b9884ad55447299e6aebb4920dedd5
otamam818 <otamam818@gmail.com>
2022-02-25 21:13:55 +1100

    Fixed errors

--- jparser/Main.java
+++ jparser/Main.java
@@ -2,9 +2,10 @@ package jparser;
 import java.util.ArrayList;
 import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public final ArrayList <Item> allItems = new ArrayList <>();
+    public static final ArrayList <Item> allItems = new ArrayList <>();
     public static final File DIR_ITEMS = new File("data/items");
 
     public static void main(String args[]) {
@@ -12,22 +13,26 @@ public class Main {
         getItems();
     }
 
-    private static String[] getItems() {
+    private static void getItems() {
         for (File currentFile : DIR_ITEMS.listFiles()) {
             String fileName = currentFile.getName();
             boolean isItemFile = !fileName.equals("README.md");
             if (isItemFile) {
-                addItem();
+                addItem(fileName);
             }
         }
-        return new String[3];
     }
-    private void addItem(String fileName) {
+
+    private static void addItem(String fileName) {
         new Thread() {
             public void run() {
                 // Variables for the Item class
-                String ID = fileName, name, description;
-                float minCostRange, maxCostRange;
+                String ID = fileName, 
+                       itemName = null, 
+                       description = null;
+                float minCostRange = -1f, maxCostRange = -1f;
+                int timesBought = -1;
+                final String SEPARATOR = " \\| ";
 
                 /* open the file and read each line 
                  * as described in the README
@@ -40,24 +45,35 @@ public class Main {
                         String itemData = fileReader.nextLine();
 
                         if (lineNumber == 1) {
-                            // Item name
+                            itemName = itemData;
                         }
+
                         else if (lineNumber == 2) {
-                            // Description
+                            description = itemData;
                         }
+
                         else if (lineNumber == 3) {
-                            // Price
+                            String[] items = itemData.split(SEPARATOR);
+                            minCostRange = Float.parseFloat(items[0]);
+                            maxCostRange = Float.parseFloat(items[1]);
                         }
+
                         else if (lineNumber == 4) {
-                            // Times bought
+                            timesBought = Integer.parseInt(itemData);
                         }
+                        lineNumber++;
                     }
+
+                    // Add Item to ArrayList based on variables obtained
+                    allItems.add(new Item(ID, itemName, description, 
+                                 minCostRange, maxCostRange, timesBought));
                 }
 
                 catch (FileNotFoundException e) {
+                    // Log the exception
                 }
             }
-        }
+        }.start();
     }
 }
 

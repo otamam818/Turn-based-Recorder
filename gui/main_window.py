66a54c19274e530171ddebfd5acf940008838914
otamam818 <otamam818@gmail.com>
2022-03-12 17:24:24 +1100

    Implemented main window (no widgets)

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,7 +1,19 @@
-import PySide6
+import sys
+from PySide6.QtWidgets import (QApplication, QLabel, QGridLayout, QLineEdit,
+                               QTextEdit, QWidget)
 
-def main():
-    print("Hello World")
+def run_window(Widget: QWidget):
+    app = QApplication(sys.argv)
+
+    window = Widget()
+    window.show()
+
+    sys.exit(app.exec())
+
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
 if __name__ == "__main__":
-    main()
+    run_window(MainWindow)
+

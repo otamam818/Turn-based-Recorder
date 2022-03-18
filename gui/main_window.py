a993a0abf73967566bb1312b1327afe0cc4a94db
otamam818 <otamam818@gmail.com>
2022-03-19 07:28:34 +1100

    Added comments

--- gui/main_window.py
+++ gui/main_window.py
@@ -3,11 +3,14 @@ from PySide6.QtWidgets import (QApplication, QLabel, QGridLayout, QLineEdit,
                                QTextEdit, QWidget)
 
 def run_window(Widget: QWidget):
+    # Initialize the app object
     app = QApplication(sys.argv)
 
+    # Initialize and show the widget
     window = Widget()
     window.show()
 
+    # Execute the widget and leave
     sys.exit(app.exec())
 
 class MainWindow(QWidget):

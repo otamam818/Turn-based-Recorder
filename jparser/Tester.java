1144250ef96843182a6226acf9bf8f5d3941b749
otamam818 <otamam818@gmail.com>
2022-03-20 23:59:20 +1100

    Added testing implementation for Item Class

--- jparser/Tester.java
+++ jparser/Tester.java
@@ -4,7 +4,6 @@ package jparser;
 public class Tester {
     public static void main(String args[]) {
         System.out.println("Testing: Item Constructor");
-        Item test = new Item("01", "Bananas", "Edible", 12f, 16f);
-        System.out.println(test.toString());
+        System.out.println(new Item("01", "Bananas", "Edible", 12f, 16f));
     }
 }

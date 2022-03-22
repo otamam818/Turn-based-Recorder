f497781ce7ce5dd752b433bea3775d5b2c0c6230
otamam818 <otamam818@gmail.com>
2022-03-22 23:32:35 +1100

    Starting placing layout classes for main window

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,6 +1,6 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QLabel, QGridLayout, QLineEdit,
-                               QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QGridLayout, QHBoxLayout,
+                               QLineEdit, QTextEdit, QWidget)
 
 def run_window(Widget: QWidget):
     # Initialize the app object
@@ -16,6 +16,12 @@ def run_window(Widget: QWidget):
 class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        layout = QHBoxLayout()
+        self.title_label = QLabel("Turn-based Recorder")
+        layout.addWidget(self.title_label)
+        self.setLayout(layout)
+        self.resize(800, 800)
 
 if __name__ == "__main__":
     run_window(MainWindow)
